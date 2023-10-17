51146e35aea1eebc43086889cd7b8a2ce10fe4df
Bryan Sánchez <bsanchez950913@gmail.com>
2023-10-17 14:48:31 -0500

    Busqueda de Datos con JOINS

--- Biblioteca_BD.sql
+++ Biblioteca_BD.sql
@@ -119,3 +119,57 @@ CREATE TABLE participantesEventos (
     PRIMARY KEY (IdEvento,IdMiembro),
     CONSTRAINT participantesEventos_id_evento_fk FOREIGN KEY (IdEvento) REFERENCES eventos(IdEventos),
     CONSTRAINT participantesEventos_id_miembro_fk FOREIGN KEY (IdMiembro) REFERENCES miembros (IdMiembros));
+		
+------------------------------------------------------
+---------------- JOINS ----------------
+-- Los libros prestados actualmente junto con los nombres de los miembros que los tienen prestados.
+SELECT prestamos.idlibro, libros.Titulo, miembros.NombreMiembro, prestamos.fechaprestamo, prestamos.fechadevolucion FROM libros 
+JOIN prestamos ON (prestamos.IdLibro = libros.IdLibros)
+JOIN miembros ON (miembros.IdMiembros = prestamos.idMiembro);
+
+-- Lista de los libros que no han sido prestados desde hace más de 6 meses.
+SELECT libros.Titulo, prestamos.FechaPrestamo 
+FROM prestamos
+JOIN libros ON (libros.IdLibros = prestamos.idLibro)
+WHERE (prestamos.FechaPrestamo < '2023-04-30');
+
+-- Cantidad total de libros escritos por cada autor.
+SELECT autores.NombreAutor "Autor", COUNT(libros.IdLibros) "Total Libros Escritos"
+FROM autores
+JOIN libros ON (libros.IdAutor = autores.IdAutores)
+GROUP BY autores.NombreAutor
+ORDER BY COUNT(libros.IdLibros) DESC;
+
+-- Libros que han sido escritos por autores de una nacionalidad específica.
+SELECT libros.Titulo "Libro", autores.NombreAutor "Autor", autores.Nacionalidad 
+FROM autores 
+JOIN libros ON (libros.IdAutor = autores.IdAutores)
+WHERE autores.Nacionalidad = 'China';
+
+-- La editorial que ha publicado más libros
+SELECT editoriales.NombreEditorial "Editorial", COUNT(libros.IdLibros) "Total_Libros_Publicados"
+FROM editoriales
+JOIN libros ON (libros.IdEditorial = editoriales.IdEditoriales)
+GROUP BY editoriales.NombreEditorial
+ORDER BY COUNT(libros.IdLibros) DESC LIMIT 1;
+
+-- Listar los miembros que han realizado reservas de libros y los libros reservados.
+SELECT reservas.Idlibro, reservas.IdMiembro,  miembros.NombreMiembro "Miembro", libros.Titulo "Titulo_Libro"
+FROM miembros
+JOIN reservas ON (reservas.IdMiembro = miembros.IdMiembros) 
+JOIN libros ON (libros.IdLibros = reservas.IdLibro)
+ORDER BY reservas.IdLibro DESC;
+
+-- Los libros que comparten una misma categoría
+SELECT categoriasLibros.NombreCategoria "Categoria", COUNT(libros.IdLibros) "Mismo_Libro"
+FROM libros
+JOIN categoriasLibros ON (categoriasLibros.IdCategoriasLibros = libros.IdLibros)
+GROUP BY categoriasLibros.NombreCategoria
+ORDER BY COUNT(libros.IdLibros) DESC;
+
+-- Listar los autores y la cantidad de libros que han escrito en una categoría específica.
+SELECT autores.NombreAutor "Autor", COUNT(libros.IdAutor) "Libros_Escritos"
+FROM autores
+JOIN libros ON (libros.IdAutor = autores.IdAutores)
+WHERE libros.Categoria = 'Technology'
+GROUP BY autores.NombreAutor;
